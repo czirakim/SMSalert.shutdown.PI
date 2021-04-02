4d664e7063971ee8ceb457f704991cf4c58332d3
mihai cziraki <58424830+czirakim@users.noreply.github.com>
2021-04-02 15:40:21 +0300

    Create vault.py

---
+++ vault.py
@@ -0,0 +1,8 @@
+#LogDNA
+apikey="adkcdlk5960vwldwldwkqwqeq11!"
+
+#Twilio
+account = 'BBCXY2324203sdddASa11213'
+token = 'gh;la=990903-03040422'
+from_number = '+1xxxxxxxxxx'
+to_number = '+1yyyyyyyyyyy'
